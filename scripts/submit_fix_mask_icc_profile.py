87786f0dc1329411c1a82ab1845ff53e9277246f
Vojtěch Cífka <550433@mail.muni.cz>
2026-04-08 12:32:10 +0200

    feat: implement submission script

---
+++ scripts/submit_fix_mask_icc_profile.py
@@ -0,0 +1,18 @@
+from kube_jobs import storage, submit_job
+
+
+submit_job(
+    job_name="tissue-classification-fix-mask-icc-profile",
+    username=...,
+    cpu=4,
+    memory="8Gi",
+    gpu=None,
+    public=False,
+    script=[
+        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/tissue-classification.git workdir",
+        "cd workdir",
+        "uv sync",
+        "uv run python -m preprocessing.fix_mask_icc_profile +experiment=...",
+    ],
+    storage=[storage.secure.DATA],
+)
