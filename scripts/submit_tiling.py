173736fdb7b4959a62dad2a266f3ab63b2c1c961
Vojtěch Cífka <550433@mail.muni.cz>
2026-04-10 17:18:25 +0200

    feat: implement WSI tiling pipeline

---
+++ scripts/submit_tiling.py
@@ -0,0 +1,17 @@
+from kube_jobs import storage, submit_job
+
+
+submit_job(
+    job_name="tissue-classification-tiling",
+    username=...,
+    cpu=8,
+    memory="32Gi",  # approximately 4GiB per process
+    public=False,
+    script=[
+        "git clone --branch feature/implement-tiling-script https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/tissue-classification.git workdir",
+        "cd workdir",
+        "uv sync",
+        "uv run -m preprocessing.tiling +experiment=...",
+    ],
+    storage=[storage.secure.DATA],
+)
