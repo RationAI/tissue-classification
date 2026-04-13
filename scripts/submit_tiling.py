034d865dcf231dd685a95047ac601e643f36680b
Vojtěch Cífka <550433@mail.muni.cz>
2026-04-13 19:37:40 +0200

    fix: remove the name and experiment from submission script

--- scripts/submit_tiling.py
+++ scripts/submit_tiling.py
@@ -3,16 +3,15 @@ from kube_jobs import storage, submit_job
 
 submit_job(
     job_name="tissue-classification-tiling",
-    username="vcifka",
+    username=...,
     cpu=8,
     memory="64Gi",
     public=False,
     script=[
         "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/tissue-classification.git workdir",
         "cd workdir",
-        "git checkout fix/speed-up-the-tiling-process",
         "uv sync",
-        "uv run -m preprocessing.tiling +experiment=preprocessing/tiling_05mpp",
+        "uv run -m preprocessing.tiling +experiment=...",
     ],
     storage=[storage.secure.DATA],
 )
