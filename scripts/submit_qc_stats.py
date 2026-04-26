2ca73fec617916fb4b92eec3dd55b2d2703d27e1
Vojtěch Cífka <550433@mail.muni.cz>
2026-04-26 21:09:45 +0200

    fix: change to the new repo url

--- scripts/submit_qc_stats.py
+++ scripts/submit_qc_stats.py
@@ -8,7 +8,7 @@ submit_job(
     memory="64Gi",
     public=False,
     script=[
-        "git clone https://gitlab.ics.muni.cz/rationai/digital-pathology/pathology/tissue-classification.git workdir",
+        "git clone https://github.com/RationAI/tissue-classification workdir",
         "cd workdir",
         "uv sync",
         "uv run -m preprocessing.qc_stats +experiment=...",
