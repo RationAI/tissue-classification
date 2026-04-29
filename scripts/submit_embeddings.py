1689d3eec5670c007952efa9eeaf02fde009d584
Vojtěch Cífka <550433@mail.muni.cz>
2026-04-29 22:48:57 +0200

    fix: tweak submission script and concurrency

--- scripts/submit_embeddings.py
+++ scripts/submit_embeddings.py
@@ -4,16 +4,15 @@ from kube_jobs import storage, submit_job
 submit_job(
     job_name="tissue-classification-embeddings",
     username=...,
-    cpu=24,
-    memory="64Gi",
-    gpu="A40",
     public=False,
+    cpu=8,
+    memory="32Gi",
+    shm="16Gi",
     script=[
         "git clone https://github.com/RationAI/tissue-classification.git workdir",
         "cd workdir",
-        "export HF_TOKEN=<YOUR_HF_TOKEN>",
         "uv sync",
-        "uv run -m preprocessing.embeddings +experiment=preprocessing/embeddings_05mpp",
+        "uv run -m preprocessing.embeddings +experiment=...",
     ],
     storage=[storage.secure.DATA, storage.secure.PROJECTS],
 )
