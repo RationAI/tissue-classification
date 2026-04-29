9bcedcf348e6ceba06c4629f4382166b7a055436
Vojtěch Cífka <550433@mail.muni.cz>
2026-04-29 21:59:26 +0200

    fix: remove name and branch

--- scripts/submit_threshold_stats.py
+++ scripts/submit_threshold_stats.py
@@ -3,12 +3,12 @@ from kube_jobs import storage, submit_job
 
 submit_job(
     job_name="tissue-classification-threshold-stats",
-    username="vcifka",
+    username=...,
     cpu=8,
     memory="64Gi",
     public=False,
     script=[
-        "git clone --branch https://github.com/RationAI/tissue-classification.git workdir",
+        "git clone https://github.com/RationAI/tissue-classification.git workdir",
         "cd workdir",
         "uv sync",
         "uv run python -m preprocessing.threshold_stats +experiment=...",
