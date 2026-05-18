847c3ccdce916155cca363c31401427acaab6c8c
Vojtěch Cífka <550433@mail.muni.cz>
2026-05-18 18:35:52 +0200

    refactor: rename configs

--- scripts/submit_train_linear_probe.py
+++ scripts/submit_train_linear_probe.py
@@ -12,7 +12,7 @@ submit_job(
         "git clone https://github.com/RationAI/tissue-classification.git workdir",
         "cd workdir",
         "uv sync",
-        "uv run python -m ml +experiment=m...  val_fold=0,1,2,3,4 model.weight_decay=0,1e-5,1e-4,1e-3,1e-2 --multirun",
+        "uv run python -m ml +experiment=... val_fold=0,1,2,3,4 model.weight_decay=0,1e-5,1e-4,1e-3,1e-2 --multirun",
     ],
     storage=[storage.secure.PROJECTS],
 )
